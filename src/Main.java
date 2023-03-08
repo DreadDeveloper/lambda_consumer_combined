55dcad40f291d395ac46f94c5c4aeff20abda11a
Abhinav Kumar <kumaraka2000@gmail.com>
2023-03-08 18:07:04 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+import java.util.function.Consumer;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Consumer<String> consumer1 = s -> System.out.println(s + " are a dumb dumb.");
+		Consumer<String> consumer2 = s -> System.out.println(s + " are a retardo.");
+		Consumer<String> consumer3 = s -> System.out.println(s + " are an idiota.");
+		consumer1.andThen(consumer2).andThen(consumer3).accept("You");
+		Consumer<String> consumer4 = consumer1.andThen(consumer2).andThen(consumer3);
+		consumer4.accept("I");
 	}
 }
